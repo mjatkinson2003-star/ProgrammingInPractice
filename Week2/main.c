052823e115c8d2ad0861feda6539297b18b461dc
MJ_Atkinson <mjatkinson2003@gmail.com>
2026-09-15 20:09:39 +0200

    Week 2 - Municipal Budget Calculator

---
+++ Week2/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+    // variables
+    float revenue;
+    float expenses;
+    float budget;
+// title
+    printf("\nMunicipality Budget Calculator\n");
+
+// prompts
+    printf("Please enter your municipality's revenue: ");
+    scanf("%f",&revenue);
+
+    printf("Please enter your municipality's expenses: ");
+    scanf("%f",&expenses);
+//result
+    budget = revenue - expenses;
+    printf("------------------------\n");
+    printf("Revenue: %.2f\n", revenue);
+    printf("Expenses: %.2f\n", expenses);
+    printf("Budget: %.2f\n", budget);
+    printf("------------------------\n");
+
+}
